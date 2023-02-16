556583fa26ea062133b6c5351beb0daf05930087
rsl150101 <dnwnd501@naver.com>
2023-02-17 01:05:21 +0900

    서버 구축

---
+++ README.md
@@ -0,0 +1,19 @@
+# 내배캠 노드 입문 과정 튜터님 해설
+
+> 게시글, 댓글 기능의 사이트 만들기 해설
+
+## 패키지
+
+```bash
+npm i express mongoose
+npm i nodemon -D
+```
+
+### 개발 환경
+
+- express
+- mongoose
+
+### 개발자 환경
+
+- nodemon
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/sparta_posts");
+const db = mongoose.connection;
+
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("✅ DB가 연결되었습니다!"));
+
+app.listen(PORT, () => {
+  console.log(`✅ 서버가 연결되었습니다! http://localhost:${PORT}`);
+});
